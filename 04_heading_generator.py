b0273354de7935f9c90d9446826779cbaf11c7c7
Uthaeus <romanlavery@gmail.com>
2018-04-22 20:57:54 -0600

    Build an html heading generator

---
+++ 04_heading_generator.py
@@ -0,0 +1,12 @@
+"""
+heading_generator(title, heading_type)
+heading_generator('Greeting', '1')
+
+=> <h1>Greeting</h1>
+"""
+
+def heading_generator(title, heading_type):
+  return f'<h{heading_type}>{title}</h{heading_type}>'
+
+
+print(heading_generator('Hello', 2))
