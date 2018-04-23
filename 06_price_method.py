e4cbef62bc015e56f3b9dffa58fa573b67be746b
Uthaeus <romanlavery@gmail.com>
2018-04-22 21:20:05 -0600

    Build a pretty price method

---
+++ 06_price_method.py
@@ -0,0 +1,10 @@
+# pretty_price(3.20, 99) => 3.99
+
+def pretty_price(gross_price, extension):
+  if isinstance(extension, int):
+    extension = extension * 0.01
+
+  return int(gross_price) + extension
+
+print(pretty_price(3.50, 0.95))
+print(pretty_price(3.50, 95))
